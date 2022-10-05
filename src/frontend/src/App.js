3825aa11eb27967b9f65dba5b6c562ada76bf032
Ninjavin <vinjain2006@gmail.com>
2022-10-05 16:37:18 +0530

    frontend v1 done

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { TeamPage } from "./pages/TeamPage";
+import { MatchPage } from "./pages/MatchPage";
 
 function App() {
   return (
-    <div className="App">
-      <TeamPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/teams/:teamName" element={<TeamPage />} />
+        </Routes>
+        <Routes>
+          <Route
+            path="/teams/:teamName/matches/:year"
+            element={<MatchPage />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
